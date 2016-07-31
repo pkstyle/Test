aa6a2871d40a1038abe107f541f862aeddec95e0
linxianbin <519913856@qq.com>
2016-07-31 22:30:02 +0800

    添加加法功能

--- src/com/dotdot/test/DiandianTao.java
+++ src/com/dotdot/test/DiandianTao.java
@@ -5,5 +5,11 @@ public class DiandianTao {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello git!!!");
+		
+	}
+	
+	public int plus(int x,int y) {
+		
+	   return x+y;
 	}
 }
